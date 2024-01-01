02ccf3f012598fb46b80cf9932445194e2e26ee9
alaa baaj <the.lost.admiral@gmail.com>
2024-01-01 21:09:01 +0300

    commit

--- lib/screen/make_a_complaint/make_a_complaint_screen.dart
+++ lib/screen/make_a_complaint/make_a_complaint_screen.dart
@@ -21,53 +21,55 @@ class MakeAComplaintScreen extends StatelessWidget {
           press: () => Get.back(),
           icon: Icons.arrow_back_ios,
           title: "تواصل معنا"),
-      body: Form(
-        key: formKey,
-        autovalidateMode: AutovalidateMode.always,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Image.asset(
-                "lib/asset/images/MakeAComplaintScreen.png",
-                width: MediaQuery.sizeOf(context).height / 3,
-                height: MediaQuery.sizeOf(context).height / 3,
-              ),
-              TextFormFieldRadius(
-                controller: subject,
-                hint: "الموضوع",
-                radius: 10,
-                line: 1,
-                validator: ValidatorManager.name,
-              ),
-              TextFormFieldRadius(
-                controller: body,
-                hint: "نص الرسالة",
-                radius: 10,
-                line: 5,
-                topPadding: 20,
-                validator: ValidatorManager.name,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: ButtonPrimary(
-                    press: () async {
-                      if (formKey.currentState!.validate()) {
-                        var res = await aComplaintController.sendMessage(
-                            subject.text, body.text);
-                        if (res) {
-                          Get.back();
-                          snackbarDef(
-                              "ملاحظة", "تم ارسال الرسالة\nشكرا لتواصلك معنا");
-                        } else {
-                          snackbarDef(
-                              "تحذير", "حدث خطأ ما الرجاء المحاولة لاحقاً");
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          autovalidateMode: AutovalidateMode.always,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Image.asset(
+                  "lib/asset/images/MakeAComplaintScreen.png",
+                  width: MediaQuery.sizeOf(context).height / 3,
+                  height: MediaQuery.sizeOf(context).height / 3,
+                ),
+                TextFormFieldRadius(
+                  controller: subject,
+                  hint: "الموضوع",
+                  radius: 10,
+                  line: 1,
+                  validator: ValidatorManager.name,
+                ),
+                TextFormFieldRadius(
+                  controller: body,
+                  hint: "نص الرسالة",
+                  radius: 10,
+                  line: 5,
+                  topPadding: 20,
+                  validator: ValidatorManager.name,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: ButtonPrimary(
+                      press: () async {
+                        if (formKey.currentState!.validate()) {
+                          var res = await aComplaintController.sendMessage(
+                              subject.text, body.text);
+                          if (res) {
+                            Get.back();
+                            snackbarDef("ملاحظة",
+                                "تم ارسال الرسالة\nشكرا لتواصلك معنا");
+                          } else {
+                            snackbarDef(
+                                "تحذير", "حدث خطأ ما الرجاء المحاولة لاحقاً");
+                          }
                         }
-                      }
-                    },
-                    text: "ارسال"),
-              )
-            ],
+                      },
+                      text: "ارسال"),
+                )
+              ],
+            ),
           ),
         ),
       ),
