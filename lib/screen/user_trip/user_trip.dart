e8a9ae550a302fb5a949f024c5ffc052e483249c
alaa baaj <the.lost.admiral@gmail.com>
2024-01-23 11:32:59 +0300

    commit

--- lib/screen/user_trip/user_trip.dart
+++ lib/screen/user_trip/user_trip.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:taxi_drive/models/show_trip.dart';
 import 'package:taxi_drive/res/color_manager.dart';
 import 'package:taxi_drive/res/font_manager.dart';
@@ -16,91 +18,176 @@ class UserTrip extends StatelessWidget {
     TripController tripController = Get.find();
     return Scaffold(
       appBar: AppBar(
-        actions: [IconButton(onPressed: (){
-          Get.back();
-        }, icon: Icon(Icons.arrow_forward_ios_outlined))],
-        title: const Text("رحلاتي", style: FontManager.w600s24cB,), centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                Get.back();
+              },
+              icon: const Icon(Icons.arrow_forward_ios_outlined))
+        ],
+        title: const Text(
+          "رحلاتي",
+          style: FontManager.w600s24cB,
+        ),
+        centerTitle: true,
       ),
       drawer: const DrawerHome(),
-      body:
-      FutureBuilder<List<ShowTrip>>(
-          future: tripController.getUserTrips(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const ProgressDef();
-            }
-            return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: SizedBox(
-                      height: 170,
-                      child:  Card(
-                        color: Colors.white,
-                        elevation: 10,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(50),
-                            ),
-                            side: BorderSide(color: ColorManager.primary, width: 1.0)
-                        ),
-
-                        child:
-                        Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: Column(children: [
-                            Row(
-                              children: [
-                                Text(' كود الرحلة :',style: FontManager.w500s17cB,),
-                                SizedBox(width: 5,),
-                                Text(snapshot.data![index].id.toString(),style: FontManager.w500s17cB, ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text(' من : ',style: FontManager.w500s17cB,),
-                                SizedBox(width: 5,),
-                                Text(snapshot.data![index].fromLate.toString(),style: FontManager.w500s17cB,),
-                                SizedBox(width: 5,),
-                                Text(snapshot.data![index].fromLong.toString(),style: FontManager.w500s17cB,),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text('الى : ',style: FontManager.w500s17cB,),
-                                SizedBox(width: 5,),
-                                Text(snapshot.data![index].toLate.toString(),style: FontManager.w500s17cB,),
-                                SizedBox(width: 5,),
-                                Text(snapshot.data![index].toLong.toString(),style: FontManager.w500s17cB,),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text('تاريخ الرحلة : ',style: FontManager.w500s17cB,),
-                                SizedBox(width: 5,),
-                                Text(snapshot.data![index].created.toString(),style: FontManager.w500s17cB,),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('المبلغ ',  style: FontManager.w400s17cP, ),
-                                SizedBox(width: 5,),
-                                Text(snapshot.data![index].price.toString(),style: FontManager.w500s17cB,),
-                              ],
-                            ),
-
-                          ]),
-
-                        ),),
-                    ),
-                  );
-                });
-
-
+      body: FutureBuilder<List<ShowTrip>>(
+        future: tripController.getUserTrips(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const ProgressDef();
           }
-          ),
-
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return FutureBuilder<Map<String, String?>>(
+                  future: getAdress(
+                      snapshot.data![index].fromLate,
+                      snapshot.data![index].fromLong,
+                      snapshot.data![index].toLate,
+                      snapshot.data![index].toLong),
+                  builder: (context, address) {
+                    if (address.connectionState == ConnectionState.waiting) {
+                      return const ProgressDef();
+                    }
+                    return Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SizedBox(
+                        height: 170,
+                        child: Card(
+                          color: Colors.white,
+                          elevation: 10,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50),
+                              ),
+                              side: BorderSide(
+                                  color: ColorManager.primary, width: 1.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    const Text(
+                                      ' كود الرحلة :',
+                                      style: FontManager.w500s17cB,
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      snapshot.data![index].id.toString(),
+                                      style: FontManager.w500s17cB,
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      ' من : ',
+                                      style: FontManager.w500s17cB,
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(address.data!['from']!),
+                                    // Text(
+                                    //   snapshot.data![index].fromLate.toString(),
+                                    //   style: FontManager.w500s17cB,
+                                    // ),
+                                    // const SizedBox(
+                                    //   width: 5,
+                                    // ),
+                                    // Text(
+                                    //   snapshot.data![index].fromLong.toString(),
+                                    //   style: FontManager.w500s17cB,
+                                    // ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'الى : ',
+                                      style: FontManager.w500s17cB,
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(address.data!['to']!)
+                                    // Text(
+                                    //   snapshot.data![index].toLate.toString(),
+                                    //   style: FontManager.w500s17cB,
+                                    // ),
+                                    // const SizedBox(
+                                    //   width: 5,
+                                    // ),
+                                    // Text(
+                                    //   snapshot.data![index].toLong.toString(),
+                                    //   style: FontManager.w500s17cB,
+                                    // ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'تاريخ الرحلة : ',
+                                      style: FontManager.w500s17cB,
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      DateFormat('yyyy/mm/dd - hh:mm').format(
+                                          snapshot.data![index].created),
+                                      style: FontManager.w500s17cB,
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      'المبلغ ',
+                                      style: FontManager.w400s17cP,
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      snapshot.data![index].price
+                                          .toInt()
+                                          .toString(),
+                                      style: FontManager.w500s17cB,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            },
+          );
+        },
+      ),
     );
+  }
+}
+
+Future<Map<String, String?>> getAdress(
+    double frla, double frlo, double tola, double tolo) async {
+  try {
+    List<Placemark> from = await placemarkFromCoordinates(frla, frlo);
+    List<Placemark> to = await placemarkFromCoordinates(tola, tolo);
+    return {
+      'from': from[0].street.toString(),
+      'to': to[0].street.toString(),
+    };
+  } catch (erorr) {
+    return {'from': '', 'to': ''};
   }
 }
