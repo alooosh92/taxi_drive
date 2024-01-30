6d926a41eaa0c92cc6881274371823a5fd854000
SheymaFathy <freelance.ss22@gmail.com>
2024-01-30 05:06:58 +0200

    commit contact us

--- lib/screen/about_us/about_us.dart
+++ lib/screen/about_us/about_us.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:taxi_drive/res/font_manager.dart';
+import 'package:taxi_drive/screen/about_us/widget/row_icons.dart';
 import 'package:taxi_drive/widget/drawer_home.dart';
 
 class AboutUs extends StatelessWidget {
@@ -24,30 +25,41 @@ class AboutUs extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: const DrawerHome(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(right: 20.0, left: 20.0, bottom: 15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-            Center(
-              child: Image.asset(
-                'lib/asset/images/about.png',
-                width: MediaQuery.sizeOf(context).height / 3,
-                height: MediaQuery.sizeOf(context).height / 3,
-              ),
-            ),
-            const Text('مرحبًا بك في شركة ARG Group Co.، كشركة رائدة في مجال تطوير البرمجيات ملتزمة بتقديم حلول مبتكرة وعالية الجودة للشركات في جميع أنحاء العالم', style: FontManager.w400s17cB,),
-            const Text('تأسست ARG في إسطنبول، تركيا في عام 2019', style: FontManager.w400s17cB,),
-            const Text('حيث كانت مهمتنا الرئيسية في ARG Group Co. هي تمكين الشركات باستخدام أحدث التكنولوجيا وتقديم حلول برمجية استثنائية', style: FontManager.w400s17cB,),
-            const Text('.نسعى لتحويل الأفكار إلى واقع، مما يمكن عملائنا من البقاء في المقدمة في المشهد الرقمي الدينامي الحالي', style: FontManager.w400s17cB,),
-            const Text('تقدم ARG  مجموعة من التطبيقات والمواقع التي تهدف لتلبية احتياجات عملائها المتنوعة من ضمنها تطبيق تكسي الذي يجسد رؤيتنا في تحسين تجربة التنقل', style: FontManager.w400s17cB,),
-            const Text('يتيح تطبيقنا للمستخدمين الاستمتاع بتجربة ركوب سهلة وآمنة، حيث يوفر أحدث التقنيات في عالم التطبيقات الذكية. يمتاز تطبيق تكسي بواجهة مستخدم سهلة الاستخدام وخدمات فعّالة تلبي احتياجات العملاء بكل كفاءة.', style: FontManager.w400s17cB,),
+      body: Column(children: [
+         Expanded(
+           child: Center(
+                  child: Image.asset(
+                    'lib/asset/images/about1.png',
+                    width: MediaQuery.sizeOf(context).height / 2,
+                    height: MediaQuery.sizeOf(context).height / 3,
+                  ),
+                ),
+         ),
+         Expanded(
+          flex: 2,
+           child: SingleChildScrollView(
+                   child: Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0, bottom: 15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+             
+              const Text('مرحبًا بك في شركة ARG Group Co.، كشركة رائدة في مجال تطوير البرمجيات ملتزمة بتقديم حلول مبتكرة وعالية الجودة للشركات في جميع أنحاء العالم', style: FontManager.w400s17cB,),
+              const Text('تأسست ARG في إسطنبول، تركيا في عام 2019', style: FontManager.w400s17cB,),
+              const Text('حيث كانت مهمتنا الرئيسية في ARG Group Co. هي تمكين الشركات باستخدام أحدث التكنولوجيا وتقديم حلول برمجية استثنائية', style: FontManager.w400s17cB,),
+              const Text('.نسعى لتحويل الأفكار إلى واقع، مما يمكن عملائنا من البقاء في المقدمة في المشهد الرقمي الدينامكيي الحالي', style: FontManager.w400s17cB,),
+              const Text('تقدم ARG  مجموعة  من التطبيقات والمواقع التي تهدف لتلبية احتياجات عملائها المتنوعة من ضمنها تطبيق تكسي الذي يجسد رؤيتنا في تحسين تجربة التنقل, يتيح تطبيقنا للمستخدمين الاستمتاع بتجربة ركوب سهلة وآمنة، حيث يوفر أحدث التقنيات في عالم التطبيقات الذكية يمتاز تطبيق تكسي بواجهة مستخدم سهلة الاستخدام وخدمات فعالة تلبي احتياجات العملاء بكل كفاءة', style: FontManager.w400s17cB,),
+              const Text('للتواصل معنا عبر مواقعنا او الاتصال ', style: FontManager.w400s17cB,),
            
-          ]),
-        ),
-      ),
+           
+            ]),
+                   ),
+                 ),
+         ),
+       const RowIcon(),
+      ],),
+     
     );
   }
 }
